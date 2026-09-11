18d682f6e4918f3a37ab094ddac00b26a516e9c7
lucasdga1 <lucas.dga1@gmail.com>
2026-09-11 16:27:06 -0300

    Ajuste API

--- src/api/run_api.py
+++ src/api/run_api.py
@@ -1,12 +1,10 @@
 import sys
 from pathlib import Path
-
-sys.path.append(str(Path(__file__).resolve().parents[1]))
-
 import os
 import uvicorn
 from main import app
-
+sys.path.append(str(Path(__file__).resolve().parents[1]))
 if __name__ == "__main__":
+
     port = int(os.environ.get("PORT", 8000))
     uvicorn.run(app, host="0.0.0.0", port=port)
